53fdac3204a1281f3af0add499264dc8b2e021b8
Michael Seifer <mikeseifer@Michaels-MacBook-Pro.local>
2020-10-12 22:11:47 -0400

    update logic file

--- Map/static/js/logic.js
+++ Map/static/js/logic.js
@@ -83,132 +83,92 @@ var icons = {
   
 };
 
-// Perform an API call to the Citi Bike Station Information endpoint
+// Perform an API call to the RESTAPI to get the crash data.
 var queryUrl = "http://127.0.0.1:5000/crashes"
 
-//d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", function(infoRes) {
-
-  // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
-  //var queryUrl = "Data.js" 
+// Parse the data in to Latitude, Longitude, and Severity for the map.
   d3.json(queryUrl, function(crashes)  {
     var lat = crashes.map(data => data.start_lat);
     var lng = crashes.map(data => data.start_lng);
-    //lat = +lat
-    //lng = +lng
-    // var mark = L.marker([
-    //   parseFloat(crashes[["start_lat"]]),
-    //   parseFloat(crashes[["start_lng"]])
-    // ]);
     var severity = crashes.map(data => data.severity);
-    //var lat = crashes[0].start_lat;
-    //var lng = crashes[0].start_lng;
-    //var severity = crashes[0].severity;
-    console.log(lat);
-    console.log(lng);
-    console.log(severity);
-      
+    var city = crashes.map(data => data.City);
+    var county = crashes.map(data => data.County);
+    //var state = crashes.map(data => data.State);
+    var weather = crashes.map(data => data.Weather_Condition);
+    var day_night = crashes.map(data => data.Civil_Twilight);
+    var time = crashes.map(data => data.Start_Time);
 
-    var stationCount = {
+    // console.log(lat);
+    // console.log(lng);
+    // console.log(severity);
+      
+// Set up empty variables to count the number of crashes.
+    var crashCount = {
       Fender_Bender: 0,
       Moderate: 0,
       Bad: 0,
       Fatal: 0
     };
-    //   //Create a InfoBox Function
-    //   function createInfoBox(bellyButtonId) {
-    //     // use d3 library to read in `samples.json`
-    //     d3.json("./samples.json").then((data) => {
-    //         // get the metadata info
-    //     var metaDataInfo = data.metadata;
-    //     console.log(metaDataInfo);
-    //     //filter meta data info by id
-    //     var resultArray = metaDataInfo.filter(object => object.id == bellyButtonId);
-    //     var result = resultArray[0];
-    //     console.log(result);
 
-    //     var infoBox = d3.select("#sample-metadata");
-    //     infoBox.html("");
+    // Initialize a crashSeverity, which will be used as a key to access the appropriate layers, icons, and severity of crashes for layer group
+    var crashSeverity;
 
-    //     Object.entries(result).forEach(([key, value]) => {
-    //         infoBox.append("h5").text(`${key}: ${value}`);
-    //     });
-    // });
-    // }
-   
-    // Create an object to keep of the number of markers in each layer
-    // var stationCount = {
-    //   COMING_SOON: 0,
-    //   EMPTY: 0,
-    //   LOW: 0,
-    //   NORMAL: 0,
-    //   OUT_OF_ORDER: 0
-    // };
-
-    // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
-    var stationStatusCode;
-
-    // Loop through the stations (they're the same size and have partially matching data)
+    // Loop through the severity data, lat & lon data.
     for (var i = 0; i < severity.length; i++) {
-      //L.marker([lat[i],lng[i]]).addTo(map);
-      // Create a new station object with properties of both station objects
+
       var station = Object.assign({}, [lat[i], lng[i], severity[i]]);
-      // var mark = L.marker(
-      //   L.latLng(
-      //     parseFloat(item["Latitude"]),
-      //     parseFloat(item["Longitude"])
-      //   )
-      // );
+    
 
 
-      console.log(severity[i]);
+      //console.log(severity[i]);
 
 
-      // If a station is listed but not installed, it's coming soon
+      // If a crash severity = 1
       if (severity[i]<2) {
-        stationStatusCode = "Fender_Bender";
+        crashSeverity = "Fender_Bender";
       }
-      // If a station has no bikes available, it's empty
+      // If a crash severity = 2
       else if (severity[i]<3) {
-        stationStatusCode = "Moderate";
+        crashSeverity = "Moderate";
       }
-      // If a station is installed but isn't renting, it's out of order
+      // If a crash severity = 3
       else if (severity[i]<4) {
-        stationStatusCode = "Bad";
+        crashSeverity = "Bad";
       }
-      // Otherwise the station is normal
+      // If a crash severity > 3
       else {
-        stationStatusCode = "Fatal";
+        crashSeverity = "Fatal";
       }
-      console.log(stationStatusCode);
-      // Update the station count
-      stationCount[stationStatusCode]++;
+      //console.log(crashSeverity);
+      // Update the crash count
+      crashCount[crashSeverity]++;
       // Create a new marker with the appropriate icon and coordinates
       var newMarker = L.marker([lat[i],lng[i]], {
-        icon: icons[stationStatusCode]
+        icon: icons[crashSeverity]
         
       });
 
       // Add the new marker to the appropriate layer
-      newMarker.addTo(layers[stationStatusCode]);
+      newMarker.addTo(layers[crashSeverity]);
 
       // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-      newMarker.bindPopup ("Details <br>" + "Latitude: " + lat[i] + "<br> Longitude: " + lng[i] + "<br>" + " Severity: " + severity[i]);
-      //newMarker.bindPopup `Latitude: ${(lat[i])}  <br> Capacity: ${(lng[i])}  <br>  Severity: ${(severity[i])}`;
+      newMarker.bindPopup ("Details <br>" + "City: " + city[i] + "<br> Weather Conditions: " + weather[i] + "<br>" + "Time: " + time[i]);
+      
     }
 
     //Call the updateLegend function, which will... update the legend!
-    updateLegend(stationCount);
+    updateLegend(crashCount);
     //updateLegend(updatedAt, stationCount);
 });
 
-//Update the legend's innerHTML with the last updated time and station count
-function updateLegend(stationCount) {
+//Update the legend's innerHTML with the last updated crash count
+function updateLegend(crashCount) {
   document.querySelector(".legend").innerHTML = [
     //"<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='fender_bender'>Fender Bender: " + stationCount.Fender_Bender + "</p>",
-    "<p class='moderate'>Moderate: " + stationCount.Moderate + "</p>",
-    "<p class='bad'>Bad: " + stationCount.Bad + "</p>",
-    "<p class='fatal'>Fatal: " + stationCount.Fatal + "</p>"
+    "<p class='fender_bender'>Fender Bender: " + crashCount.Fender_Bender + "</p>",
+    "<p class='moderate'>Moderate: " + crashCount.Moderate + "</p>",
+    "<p class='bad'>Bad: " + crashCount.Bad + "</p>",
+    "<p class='fatal'>Fatal: " + crashCount.Fatal + "</p>"
     
   ].join("");
 }
