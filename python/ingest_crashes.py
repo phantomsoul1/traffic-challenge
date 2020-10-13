c7fc87e37248a84776e7dfa46b64ce70760882d8
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-10-12 20:40:46 -0400

    Updated Crashes data source

--- python/ingest_crashes.py
+++ python/ingest_crashes.py
@@ -5,53 +5,52 @@ import sqlite3
 from sqlite3 import Error
 
 def main():
+
+    file_names = ['traffic_2019_1h.csv', 'traffic_2019_2h.csv']
+
     # get absolute path present working directory
     # (this is needed to find other files in this project, like CSV and DB)
     pwd_path = os.path.abspath(os.path.dirname(__file__))
     
-    # get path to the source CSV file and read to dataframe
-    crash_path = os.path.join(pwd_path, '../csvfiles/traffic_crashes.csv')
-    crashes = pd.read_csv(crash_path)
+    for file_name in file_names:
 
-    # clean up column names to make downstream API and JS easier to work with
-    crashes.columns = ['id', 'source', 'tmc', 'severity', 'start_time', 'end_time',
-       'start_lat', 'start_lng', 'end_lat', 'end_lng', 'distance_mi',
-       'description', 'number', 'street', 'side', 'city', 'county', 'state',
-       'zipcode', 'country', 'timezone', 'airport_code', 'weather_timestamp',
-       'temperature_f', 'wind_chill_f', 'humidity', 'pressure_in',
-       'visibility_mi', 'wind_direction', 'wind_speed_mph',
-       'precipitation_in', 'weather_condition', 'amenity', 'bump', 'crossing',
-       'give_way', 'junction', 'no_exit', 'railway', 'roundabout', 'station',
-       'stop', 'traffic_calming', 'traffic_signal', 'turning_loop',
-       'sunrise_sunset', 'civil_twilight', 'nautical_twilight',
-       'astronomical_twilight']
+        # get path to the source CSV file and read to dataframe
+        crash_path = os.path.join(pwd_path, f'../csvfiles/{file_name}')
+        crashes = pd.read_csv(crash_path)
 
-    # get path location for the sqlite DB file
-    db_path = os.path.join(pwd_path, '../db/crashes.db')
-    
-    #initialize a connection object for SQL
-    conn = None
+        # clean up column names to make downstream API and JS easier to work with
+        crashes.columns = ['source', 'severity', 'start_time', 'end_time',
+        'start_lat', 'start_lng', 'city', 'county', 'state',
+        'temperature_f', 'wind_chill_f', 'humidity', 'pressure_in',
+        'visibility_mi', 'wind_speed_mph', 'precipitation_in', 
+        'weather_condition', 'civil_twilight', 'year', 'month']
 
-    try:
+        # get path location for the sqlite DB file
+        db_path = os.path.join(pwd_path, '../db/crashes.db')
+        
+        #initialize a connection object for SQL
+        conn = None
 
-        # connect to sqlite db, creating it if necessary
-        conn = dbutil.connect(db_path)
+        try:
 
-        # write the dataframe to the sqlite file, replacing any existing records
-        # (this setup will help scale the source dataset size as we get ready to "go live")
-        crashes.to_sql(dbutil.get_crash_table(), con=conn, if_exists='replace')
+            # connect to sqlite db, creating it if necessary
+            conn = dbutil.connect(db_path)
 
-    except Error as e:
-        # print any sqlite errors that occur
-        print(e)
+            # write the dataframe to the sqlite file, replacing any existing records
+            # (this setup will help scale the source dataset size as we get ready to "go live")
+            crashes.to_sql(dbutil.get_crash_table(), con=conn, if_exists='replace')
 
-    finally:
-        # if we have an open connection object, close it
-        if (conn != None):
-            conn.close()
+        except Error as e:
+            # print any sqlite errors that occur
+            print(e)
 
-    # report how many crashes are added
-    print(f'{crashes.count()} crashes added.')
+        finally:
+            # if we have an open connection object, close it
+            if (conn != None):
+                conn.close()
+
+        # report how many crashes are added
+        print(f'{crashes.count()} crashes added.')
 
 # run 'main' if this is the file being run
 # (this is python's standard entry point pattern)
