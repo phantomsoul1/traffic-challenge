318edcd7cfb305818e35623f8d5f28ed979aa44c
Jake Byford <jake_byford@outlook.com>
2020-10-14 13:57:12 -0400

    Bubble Chart

---
+++ html/static/js/jake_viz.js
@@ -0,0 +1,196 @@
+function createCharts(myId) {
+  d3.json("http://127.0.0.1:5000/crashes").then(function(data) {
+  console.log(data);
+  
+  var crashes = data;
+  var severity = crashes.map(object => object.severity);
+  var counties = crashes.map(object => object.county);
+  console.log(counties);
+
+  var sum = 0;
+  
+  var capeMay = [];
+  var mercer = [];
+  var ocean = [];
+  var monmouth = [];
+  var camden = [];
+  var cumberland = [];
+  var bergen = [];
+  var passiac = [];
+  var sussex = [];
+  var morris = [];
+  var gloucester = [];
+  var hudson = [];
+  var warren = [];
+  var union = [];
+  var salem = [];
+  var burlington = [];
+  var somerset = [];
+  var middlesex = [];
+  var essex = [];
+  var hunterdon = [];
+  var atlantic = [];
+
+counties.forEach(function(county){
+  sum += county;
+
+  if (county === "Cape May") {
+    capeMay.push(county);
+  }
+  else if (county === "Mercer") {
+    mercer.push(county);
+  }
+  else if (county === "Ocean") {
+    ocean.push(county);
+  }
+  else if (county === "Monmouth") {
+    monmouth.push(county);
+  }
+  else if (county === "Camden") {
+    camden.push(county);
+  }
+  else if (county === "Cumberland") {
+    cumberland.push(county);
+  }
+  else if (county === "Bergen") {
+    bergen.push(county);
+  }
+  else if (county === "Passiac") {
+    passiac.push(county);
+  }
+  else if (county === "Sussex") {
+    sussex.push(county);
+  }
+  else if (county === "Morris") {
+    morris.push(county);
+  }
+  else if (county === "Hudson") {
+    hudson.push(county);
+  }
+  else if (county === "Gloucester"){
+    gloucester.push(county);
+  }
+  else if (county === "Warren"){
+    warren.push(county);
+  }
+  else if (county === "Union"){
+    union.push(county);
+  }
+  else if (county === "Salem"){
+    salem.push(county);
+  }
+  else if (county === "Burlington"){
+    burlington.push(county);
+  }
+  else if (county === "Somerset"){
+    somerset.push(county);
+  }
+  else if (county === "Middlesex"){
+    middlesex.push(county);
+  }
+  else if (county === "Essex"){
+    essex.push(county);
+  }
+  else if (county === "Hunterdon"){
+    hunterdon.push(county);
+  }
+  else {
+    atlantic.push(county);
+  }
+});
+
+var numCapeMay = capeMay.length;
+var numMercer = mercer.length;
+var numOcean = ocean.length;
+var numMonmouth = monmouth.length;
+var numCamden = camden.length;
+var numCumberland = cumberland.length;
+var numBergen = bergen.length;
+var numPassiac = passiac.length;
+var numSussex = sussex.length;
+var numMorris= morris.length;
+var numGloucester = gloucester.length;
+var numHudson = hudson.length;
+var numWarren = warren.length;
+var numUnion = union.length;
+var numSalem = salem.length;
+var numBurlington = burlington.length;
+var numSomerset = somerset.length;
+var numMiddlesex= middlesex.length;
+var numEssex = essex.length;
+var numHunterdon = hunterdon.length;
+var numAtlantic = atlantic.length;
+  
+  console.log(numCapeMay);
+  console.log(numMercer);
+  console.log(numOcean);
+  console.log(numMonmouth);
+  console.log(numCamden);
+  console.log(numCumberland);
+  console.log(numBergen);
+  console.log(numPassiac);
+  console.log(numSussex);
+  console.log(numMorris);
+  console.log(numGloucester);
+  console.log(numHudson);
+  console.log(numWarren);
+  console.log(numUnion);
+  console.log(numSalem);
+  console.log(numBurlington);
+  console.log(numSomerset);
+  console.log(numMiddlesex);
+  console.log(numEssex);
+  console.log(numHunterdon);
+  console.log(numAtlantic);
+
+  var countyLengths = [
+        numCapeMay,
+        numMercer,
+        numOcean,
+        numMonmouth,
+        numCamden,
+        numCumberland,
+        numBergen,
+        numPassiac,
+        numSussex,
+        numMorris,
+        numGloucester,
+        numHudson,
+        numWarren,
+        numUnion,
+        numSalem,
+        numBurlington,
+        numSomerset,
+        numMiddlesex,
+        numEssex,
+        numHunterdon,
+        numAtlantic
+  ];
+
+  console.log(countyLengths); 
+
+
+
+  var bubbleTrace = {
+    x: counties,
+    y: countyLengths,
+    mode: 'markers',
+    marker: {
+      size: countyLengths,
+      color: counties,
+      colorscale: "Bluered"
+    }
+    };
+
+  var bubbleData = [bubbleTrace];
+
+  var bubbleLayout = {
+    title: "Bubble Chart"
+  };
+
+  Plotly.newPlot("myDiv", bubbleData, bubbleLayout);
+
+  }); 
+  
+}
+createCharts();
