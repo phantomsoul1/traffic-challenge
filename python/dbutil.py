429ea9bb188497d80aec2233d5d9656614d30fa0
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-10-13 20:22:05 -0400

    Adds queries by county and month

--- python/dbutil.py
+++ python/dbutil.py
@@ -33,12 +33,33 @@ def dict_factory(cursor, row):
 
 # get all crashes in the database
 def get_all_crashes(conn):
+    return get_crashes(conn, None, None)
+
+def get_crashes(conn, county, month):
     """
     conn: sqlite3 connection object
     """
     
     # sql statement, incorporating the table name described above
+    sql_where = ""
+    if (county):
+        sql_where += f" WHERE LOWER(county)='{county}'"
+
+    if (month):
+        if (len(sql_where) > 0):
+            sql_where += f" AND month={month}"
+        else:
+            sql_where += f" WHERE month={month}"
     sql = f"SELECT * FROM {crash_table}"
+
+    sql_limit = ""
+    if (county is None and month is None):
+        sql_limit = " ORDER BY start_time DESC LIMIT 1000"
+
+    sql = f"SELECT * FROM {crash_table}{sql_where}{sql_limit}"
+
+    print(sql)
+    rows = None
 
     try:
 
--- python/publish_crashes.py
+++ python/publish_crashes.py
@@ -1,7 +1,7 @@
 import dbutil
 import os.path
 import sqlite3
-from flask import Flask, redirect, Response, json
+from flask import Flask, redirect, request, Response, json, url_for
 from flask_cors import CORS
 from sqlite3 import Error
 
@@ -11,22 +11,44 @@ cors = CORS(app)
 
 #global crash list - we don't need to reload this every time its requested
 crash_list = None
+county = None
+month = None
+
+def validate():
+    global county, month
+    
+    if (request.method == 'GET'):
+        c = request.args.get("county")
+        if (c):
+            c = c.lower()
+
+        m = request.args.get("month")
+        if (m):
+            m = m.lower()
+
+        if (crash_list and c == county and m == month):
+            return True
+
+    county = c
+    month = m
+
+    return False
 
 # default route; redirect to refresh if crash list is not define or to crashes otherwise
 @app.route('/')
 def home():
     # redirect to crashes if defined
-    if (crash_list):
-        return redirect("/crashes")
+    if (validate()):
+        return redirect(url_for("crashes", county = county, month = month))
     
     # otherwise refresh the crash list first
-    return redirect("/refresh")
+    return redirect(url_for("refresh", county = county, month = month))
 
 # return the crash list
 @app.route('/crashes')
 def crashes():
 
-    if (crash_list):
+    if (validate()):
         return Response(json.dumps(crash_list),  mimetype='application/json')
 
     # return the crash list as a json object
@@ -38,7 +60,7 @@ def refresh():
 
     # (this is where the sausage is made...)
     # set crash_list to global so we update the global one for the other routes
-    global crash_list
+    global county, month, crash_list
 
     # get the path to the crashes database
     # (pwd_path enables python to find files within this project)
@@ -54,7 +76,7 @@ def refresh():
         conn = dbutil.connect(db_path)
 
         # get all crashes from sqlite
-        crash_list = dbutil.get_all_crashes(conn)
+        crash_list = dbutil.get_crashes(conn, county, month)
 
     except Error as e:
 
@@ -67,7 +89,7 @@ def refresh():
             conn.close()
 
     # redirect to crashes to show the result
-    return redirect('/crashes')
+    return redirect(url_for('crashes', county = county, month = month))
 
 if __name__ == '__main__':
     app.run()
