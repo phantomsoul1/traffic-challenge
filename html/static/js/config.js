7f6eed2dc402f77e13537ba1253445a5a8eeec00
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-10-25 20:51:50 -0400

    Update config.js
    Please make sure this file is added to skip-worktree after cloning this repository. In a command line session, navigate to this cloned repository's html/static/js directory and run this command, without the double quotes: "git update-index --skip-worktree config.js
    
    This will ensure that when you add your own API key or switch between base url servers, nothing gets accidentally committed, disrupting other contributors.

--- html/static/js/config.js
+++ html/static/js/config.js
@@ -1,4 +1,4 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiamFrZWJ5Zm9yZCIsImEiOiJja2ZweDlxbmUwZmt4MnpydjA0eTBmcWl5In0.Lbl40gv3w_jCljyJn1MJZg";
+const API_KEY = "ADD MAPBOX API KEY HERE";
 const BASE_URL = 'https://phantomsoul-traffic-challenge.herokuapp.com/';
+//const BASE_URL = 'http://127.0.0.1:5000/';
 
